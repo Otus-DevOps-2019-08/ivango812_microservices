3a013a2829bf4729d385928d5ff19f78807eb0f0
ivango812 <ivan@newbusinesslogic.ru>
2019-11-17 22:47:09 +0300

    Added playbook to run reddit in docker container

--- docker-monolith/infra/main.tf
+++ docker-monolith/infra/main.tf
@@ -5,11 +5,11 @@ provider "google" {
 }
 
 resource "google_compute_instance" "docker-host" {
-  count = var.instances_count
-  name = "docker-host-${count.index}"
+  count        = var.instances_count
+  name         = "docker-host-${count.index}"
   machine_type = var.machine_type
-  zone = var.zone
-  tags = ["docker-machines"]
+  zone         = var.zone
+  tags         = ["docker-machines"]
   boot_disk {
     initialize_params {
       image = var.disk_image
@@ -17,19 +17,48 @@ resource "google_compute_instance" "docker-host" {
   }
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+    }
   }
   metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
+  // connection {
+  //   type  = "ssh"
+  //   host  = self.network_interface[0].access_config[0].nat_ip
+  //   user  = "appuser"
+  //   agent = false
+  //   private_key = file(var.private_key_path)
+  // }
+  // provisioner "file" {
+  //   source      = "${path.module}/files/mongod.conf"
+  //   destination = var.enable_provisioner == true ? "/tmp/mongod.conf": "/dev/null"
+  // }
+  // provisioner "remote-exec" {
+  //   script = var.enable_provisioner == true ? "${path.module}/files/deploy.sh": ""
+  // }
+  // provisioner "local-exec" {
+  //   when = "destroy"
+  //   command = "ssh-keygen -R ${self.network_interface[0].access_config[0].nat_ip}"
+  // }  
 }
 
 resource "google_compute_firewall" "firewall_docker" {
-  name = "docker-machines"
+  name    = "docker-machines"
   network = "default"
   allow {
     protocol = "tcp"
-    ports = ["2376"]
+    ports    = ["2376"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "firewall_reddit" {
+  name    = "reddit-http"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
   }
   source_ranges = ["0.0.0.0/0"]
 }
--- docker-monolith/infra/variables.tf
+++ docker-monolith/infra/variables.tf
@@ -1,34 +1,37 @@
-variable project {
+variable "project" {
   description = "Project ID"
 }
-variable region {
+
+variable "region" {
   description = "Region"
+
   # Значение по умолчанию
   default = "europe-west1"
 }
 
-variable zone {
+variable "zone" {
   description = "Zone for instance"
   default     = "europe-west1-b"
 }
 
-variable machine_type {
+variable "machine_type" {
   description = "Machine type for instance"
   default     = "n1-standard-1"
-} 
+}
 
-variable disk_image {
+variable "disk_image" {
   description = "Disk image"
 }
 
-variable public_key_path {
+variable "public_key_path" {
   description = "Path to the public key used for ssh access"
 }
 
-variable private_key_path {
+variable "private_key_path" {
   description = "Path to the private key used for ssh conection"
 }
 
-variable instances_count {
+variable "instances_count" {
   default = 1
 }
+
