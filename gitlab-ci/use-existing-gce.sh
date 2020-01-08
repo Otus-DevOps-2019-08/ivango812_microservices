2efca274c3f274d22aabcb007728264ea8361f96
Ivan Gorban <ivan@newbusinesslogic.ru>
2020-01-08 22:58:16 +0300

    Revert "Merge branch 'master' into monitoring-2"
    This reverts commit 508d839b2983508b8e1aaba99fcc3cdda1a1613d, reversing
    changes made to 16ad3632073b3bc9f4261373eb300193e6ec33a1.

--- gitlab-ci/use-existing-gce.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker-machine create --driver google \
-  --google-project $PROJECT_ID \
-  --google-zone europe-west1-b \
-  --google-use-existing \
-  gitlab-ce
-
-eval $(docker-machine env gitlab-ce)
